b05e6b2e2455a7e6124dae54c78d4ab4863c8052
AIITV <113987436+AIITV@users.noreply.github.com>
2022-11-07 16:55:07 +0100

    Add files via upload

--- yt_downloader/installer.sh
+++ yt_downloader/installer.sh
@@ -3,13 +3,10 @@
 sudo apt update -y
 sudo apt upgrade -y
 
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt-get update
-sudo apt-get remove python3
-sudo apt-get install python3.5
+sudo apt install python3 -y
 sudo apt install python3-pip -y
 
-pip install tkinter
+pip install tk
 pip install pytube
 
 python3 download.py
