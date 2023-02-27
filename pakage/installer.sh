6f0ec35ea2716a095e14d42830a1967dfeba5906
AIITV <113987436+AIITV@users.noreply.github.com>
2023-02-27 12:54:53 +0100

    installer

---
+++ pakage/installer.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+apt-get update -y
+apt-get upgrade -y
+apt-get install sudo -y
+
+sudo apt update -y
+sudo apt upgrade -y
+
+sudo apt install github -y
+sudo apt install nano -y
+
+sudo apt install wireshark -y
+sudo apt install hashcat -y
+
+clear
